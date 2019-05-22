81f87a64617348d6364aa09440cceef63aa6844c
Elijah Paminsan <e.paminsan@gmail.com>
2019-05-22 13:51:48 -0700

    Converted functions from ES5 to ES6 arrow syntax

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,34 @@
 // };
 // myFunction();
 
+const myFunct = () => {console.log("Function was invoked!");}
+myFunct();
+
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunct = (param) => {return param;}
+anotherFunct("Example2");
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const addition = (p1, p2) => {return p1 + p2};
+addition(5, 5);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const subtraction = (p3, p4) => {return p3 - p4};
+subtraction(6, 5);
 
 
 // Stretch
@@ -28,3 +42,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+arr = [1, 2, 3, 4];
+const trip = arr.map((num) => {return num * 3});
+console.log(trip)
